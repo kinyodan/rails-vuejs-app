5e8e68d6853ed656a4bd20f5c7f4c06479c0f35b
Kyle Geske <stungeye@gmail.com>
2019-11-11 22:02:45 -0600

    Fixed the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
   resources :to_dos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'about', to: 'pages#about', as: 'about'
 end
